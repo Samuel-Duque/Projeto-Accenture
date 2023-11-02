284e1fc9138557065991db29bae2b66255f889bc
Samuel-Duque <samuellocatel@gmail.com>
2023-11-02 13:13:32 -0300

    Updating components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
+    import Button from '$lib/components/Button.svelte';
     import Header from '$lib/components/Header.svelte';
+    import SwitchButton from '$lib/components/SwitchButton.svelte';
+    import InputServiceLocation from '$lib/components/input-service-location.svelte';
 </script>
+
+
 <Header>
-<slot/>
+    <slot />
 </Header>
+
+<InputServiceLocation>
+    <slot />
+</InputServiceLocation>
+
+<SwitchButton> 
+    <slot />
+</SwitchButton>
+ 
